6561df1943da0d209a666357184a8a7ac1d42155
m-xuanquy <hi.maixuanquy@gmail.com>
2024-12-21 16:26:31 +0700

    fix: modify for deploy

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(express.json());    // allows us to accept JSON data in the req.body
 
 app.use("/api/posts", postRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res)=> {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//     app.get("*", (req, res)=> {
+//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//     })
+// }
 
 app.listen(PORT, () => {
     connectDB();
