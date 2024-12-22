0a155ed412abd23179d9dc443e38e3dd14f7fd43
m-xuanquy <hi.maixuanquy@gmail.com>
2024-12-22 09:04:02 +0700

    fix: interact with technews_db

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const dbURI = `${process.env.MONGODB_URI}${process.env.MONGODB_DATABASE_NAME}`;
+        const conn = await mongoose.connect(dbURI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
