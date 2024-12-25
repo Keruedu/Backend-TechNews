b6a8d0c156eee1137f8ddbc1cc26c52b29962e0f
m-xuanquy <hi.maixuanquy@gmail.com>
2024-12-25 12:06:06 +0700

    Add searchPosts POST method with filter, sorting and pagination

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createPost, deletePost, getPosts, updatePost } from '../controllers/postController.js';
+import { createPost, deletePost, getPosts, updatePost, searchPosts } from '../controllers/postController.js';
 
 const router = express.Router();
 
+router.post("/search", searchPosts); // New route for searching posts with filters and sorting
 router.get("/", getPosts);
 router.post("/", createPost);
 router.put("/:id", updatePost);
